2bd7988e437af924bf92d405c1acba9d9ef30d3d
CarrotHands <akekajoo@gmail.com>
2022-11-25 22:28:50 -0800

    added playRound func

--- rps.js
+++ rps.js
@@ -10,3 +10,37 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+function getPlayerFunction() {
+    return "rock";
+}
+
+function playRound(player, computer) {
+    let output =  "Player played " + player + " and computer played " + computer + ". \n")
+    if (player == computer) {
+        output += "It's a tie.";
+    }
+
+    switch (player + computer) {
+        case "rockpaper":
+            output += "Computer wins.";
+            break;
+        case "rockscissors":
+            output += "Player wins.";
+            break;
+        case "paperrock":
+            output += "Player wins.";
+            break;
+        case "paperscissors":
+            output += "Computer wins.";
+            break;
+        case "scissorsrock":
+            output += "Computer wins";
+            break;
+        case "scissorspaper":
+            output += "Player wins";
+            break;    
+    }
+
+    return output;
+}

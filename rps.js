90aef3ce525e72ca23fb4e522a7f6e4e62f6f558
CarrotHands <akekajoo@gmail.com>
2022-11-27 23:46:16 -0800

    added ui features

--- rps.js
+++ rps.js
@@ -1,6 +1,24 @@
-function getComputerChoice() {
-    let choice = Math.random() * 3;
+let playerWins = 0;
+let computerWins = 0;
 
+window.onload = function(e) {    
+    const rock = document.getElementById("rock");
+    const paper = document.getElementById("paper");
+    const scissors = document.getElementById("scissors");
+
+    rock.addEventListener("click", () => getPlayerChoice("rock"));
+    paper.addEventListener("click", () => getPlayerChoice("paper"));
+    scissors.addEventListener("click", () => getPlayerChoice("scissors"));
+
+    document.getElementById("playerWins").innerText = playerWins;
+    document.getElementById("computerWins").innerText = computerWins;
+}
+
+
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    
     switch (choice) {
         case 0:
             return "rock";        
@@ -11,20 +29,15 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    play = prompt().toLowerCase;
-    if (play == "rock" || play == "paper" || play == "scissors") {
-        return play;
-    }
-    else {
-        console.log("enter rock paper or scissors please");
-        return getPlayerFunction();
-    }
-    
+function getPlayerChoice(play) {
+    let computerPlay = getComputerChoice();
+    playRound(play, computerPlay);   
+    document.getElementById("computerPlayed").innerText = "Computer played " + computerPlay;
 }
 
 function playRound(player, computer) {
-    let output =  "Player played " + player + " and computer played " + computer + ". \n")
+    //console.log("Player played " + player + " and computer played " + computer + ". \n");
+    let output = "";
     if (player == computer) {
         output += "It's a tie.";
     }
@@ -32,23 +45,41 @@ function playRound(player, computer) {
     switch (player + computer) {
         case "rockpaper":
             output += "Computer wins.";
+            computerWins++;
             break;
         case "rockscissors":
             output += "Player wins.";
+            playerWins++;
             break;
         case "paperrock":
             output += "Player wins.";
+            playerWins++;
             break;
         case "paperscissors":
             output += "Computer wins.";
+            computerWins++;
             break;
         case "scissorsrock":
             output += "Computer wins";
+            computerWins++;
             break;
         case "scissorspaper":
             output += "Player wins";
+            playerWins++;
             break;    
     }
 
-    return output;
+    document.getElementById("playerWins").innerText = playerWins;
+    document.getElementById("computerWins").innerText = computerWins;
+
+    if (playerWins == 5) {
+        document.getElementById("winner").innerText = "The player wins";
+        location.reload();
+    }
+    else if (computerWins == 5) {
+        document.getElementById("winner").innerText = "The computer wins";
+        location.reload();
+    }
+
 }
+
